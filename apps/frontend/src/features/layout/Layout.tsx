ec3c84176da52c5e15eac0aec56fa3ad164953f6
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2023-12-28 22:40:17 -0300

    frontend react routes and primereact setup

---
+++ apps/frontend/src/features/layout/Layout.tsx
@@ -0,0 +1,9 @@
+import { Router } from "react-router-dom";
+
+const Layout = (router: Router) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Layout router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
