7a5efbf7b7b64aa3c8118a87abd2be45a34aa34e
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2024-01-17 00:30:06 -0300

    chore: created logic for message senders

--- apps/frontend/src/features/messages/MessagesInputBox.tsx
+++ apps/frontend/src/features/messages/MessagesInputBox.tsx
@@ -1,5 +1,9 @@
 import { Card } from "primereact/card"
 import { defaultTheme } from "../../themes"
+import { graphql } from "relay-runtime"
+import { useMutation } from "react-relay"
+import { useContext, useState } from "react"
+import { ProfileContext } from "../profiling/ProfileContext"
 
 const messageInputBoxStyle = {
     height: defaultTheme.shapes.messageBoxHeight,
@@ -16,13 +20,36 @@ const messageTextBoxStyle = {
     border: 'none',
 }
 
+const MessagesInputBoxMutation = graphql`
+    mutation MessagesInputBoxMutation($input: MessageInput) {
+        createPendingMessage(input: $input)
+    }
+`;
+
 const MessagesInputBox = () => {
+    const [commitMutation] = useMutation(MessagesInputBoxMutation);
+    const { contextValue } = useContext(ProfileContext);
+    const [message, setMessage] = useState('');
+
     return (
         <Card style={messageInputBoxStyle}>
         <input 
             style={messageTextBoxStyle} 
             type="text"
-            placeholder="Type a message..." />
+            placeholder="Type a message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                    commitMutation({
+                        variables: {
+                            content: message,
+                            receiverId: '',
+                            senderId: contextValue.uuid
+                        }
+                    });
+                }
+            }} />
         </Card>
     )
 }
