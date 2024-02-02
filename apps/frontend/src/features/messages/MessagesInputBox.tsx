1eb8603c7904527a8a175fd6e7f9452517f83925
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2024-02-02 03:14:21 -0300

    chore: normalized messaging errors and behaviors + error handlers

--- apps/frontend/src/features/messages/MessagesInputBox.tsx
+++ apps/frontend/src/features/messages/MessagesInputBox.tsx
@@ -2,7 +2,7 @@ import { Card } from "primereact/card"
 import { defaultTheme } from "../../themes"
 import { graphql } from "relay-runtime"
 import { useMutation } from "react-relay"
-import { SetStateAction, useContext, useState } from "react"
+import { SetStateAction, createRef, useContext, useState } from "react"
 import { ProfileContext } from "../profiling/ProfileContext"
 import { toast } from "react-toastify"
 import { Message } from "./MessagesPresets"
@@ -36,6 +36,7 @@ const MessagesInputBox = (props: MessagesInputBoxProps) => {
     const [commitMutation] = useMutation(MessagesInputBoxMutation);
     const { contextValue } = useContext(ProfileContext);
     const [message, setMessage] = useState('');
+    const inputRef = createRef<HTMLInputElement>();
 
     return (
         <Card style={messageInputBoxStyle}>
@@ -43,6 +44,7 @@ const MessagesInputBox = (props: MessagesInputBoxProps) => {
             style={messageTextBoxStyle} 
             type="text"
             placeholder="Type a message..."
+            ref={inputRef}
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             onKeyDown={(e) => {
@@ -75,6 +77,8 @@ const MessagesInputBox = (props: MessagesInputBoxProps) => {
                             token: localStorage.getItem('token')
                         },
                         onCompleted: (_, errors) => {
+                            setMessage('');
+
                             errors?.forEach(error => {
                                 toast.error('Can not send message: ' + error.message, {
                                     position: "top-right",
@@ -89,8 +93,6 @@ const MessagesInputBox = (props: MessagesInputBoxProps) => {
                             });
 
                             if (!errors) {
-                                setMessage('');
-
                                 // Actually updates the send message to delivered
                                 props.setMessages(current => {
                                     return current.map(message => {
@@ -103,8 +105,21 @@ const MessagesInputBox = (props: MessagesInputBoxProps) => {
 
                                         return message
                                     })
-                                })
+                                });
+
+                                return;
                             }
+
+                            // Removes the problematic message
+                            props.setMessages(current => {
+                                return current.filter(message => {
+                                    if (message.uuid == localId) {
+                                        return false;
+                                    }
+
+                                    return true;
+                                })
+                            });
                         }
                     });
                 }
